3dcd97f43b8357ba356cb633ab7afeefd84e1262
Marek Klement <marek.klement@bcvsolutions.eu>
2020-01-23 13:57:44 +0100

    First commit

---
+++ src/components/Headers/IndexHeader.js
@@ -0,0 +1,82 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+import Img from 'react-image';
+import logo from '../../images/logo-bitva.svg';
+
+// core components
+
+function IndexHeader() {
+  return (
+    <>
+      <div
+        className="page-header section-dark"
+        style={{
+          backgroundImage:
+            "url(" + require("assets/img/background.jpg") + ")"
+        }}
+      >
+        <div className="filter" />
+        <div className="content-center">
+          <Container>
+            <div className="title-brand">
+              <Img
+                style={{width: 400, height: 400}}
+                src={logo} />
+              <div className="fog-low">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div>
+              <div className="fog-low right">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div>
+            </div>
+            <h2 className="presentation-subtitle text-center">
+              Backbone Bitva o Prahu!
+            </h2>
+          </Container>
+        </div>
+        <div
+          className="moving-clouds"
+          style={{
+            backgroundImage: "url(" + require("assets/img/clouds.png") + ")"
+          }}
+        />
+        <h6 className="category category-absolute">
+          Designed and coded by{" "}
+          <a
+            href="https://www.creative-tim.com?ref=pkr-index-page"
+            target="_blank"
+          >
+            <img
+              alt="..."
+              className="creative-tim-logo"
+              src={require("assets/img/creative-tim-white-slim2.png")}
+            />
+          </a>
+        </h6>
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
