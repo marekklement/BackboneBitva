98ce4f97c03bad6f1a655b26279cbf7b452bc3e2
Marek Klement <marek.klement@bcvsolutions.eu>
2020-01-24 19:31:38 +0100

    Added text to about

--- src/components/About/AboutUs.js
+++ src/components/About/AboutUs.js
@@ -5,7 +5,7 @@ class AboutUs extends Component{
   render() {
     return (
         <div>
-          akajakjkdjd
+           <p>Z utkání mezi dvěma pražskými týmy, tedy Engineers Prague a UK Hockey Prague, vzešla postupem času tradiční <span style="font-weight: bold;">BITVA O PRAHU</span>, která se v rámci univerzitního hokeje řadí mezi TOP zápasy.</p><p>K pražskému derby na ledě si představte vyprodaný stadion, různorodý doprovodný program, soutěže o ceny, významné osobnosti jako čestné hosty, nebo studentské pochody jednotlivých univerzit na zimák, díky kterým se naladí ta správná atmosféra ještě před startem samotného duelu.</p><p>Tradičním místem pro derby je Zimní stadion Hvězda, kde inženýři odehrávají všechny domácí zápasy. V roce 2018 se povedlo po 20 letech zimák vyprodat právě díky tomuto utkání. Když 800 fanoušků zavítalo na legendární bitvu, tak stadion skutečně praskal ve švech!</p><p><span style="font-weight: bold;">Letos, konkrétně 2. března 2020, se Bitva o Prahu přesouvá do prostor Tipsport Areny a poprvé bude vysílána také živě v České televizi!</span></p><p><span style="font-weight: bold;"><iframe src="//www.youtube.com/embed/wjqHPCT1aw4" width="640" height="360" frameborder="0"></iframe><br></span></p>
         </div>
     );
   }
