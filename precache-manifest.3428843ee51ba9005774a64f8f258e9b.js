1370eba1ce292972644d6d67d6e3dcb22abea0db
Marek Klement <marek.klement@bcvsolutions.eu>
2020-01-26 13:06:53 +0100

    Updates

---
+++ precache-manifest.3428843ee51ba9005774a64f8f258e9b.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439492556d3d114c9a82e52c16cdc7d1",
+    "url": "/paper-kit-react-master/index.html"
+  },
+  {
+    "revision": "d2a57ce796d43014475b",
+    "url": "/paper-kit-react-master/static/css/main.350a75ee.chunk.css"
+  },
+  {
+    "revision": "10df74863ef3cfcae9db",
+    "url": "/paper-kit-react-master/static/js/2.a5c9b508.chunk.js"
+  },
+  {
+    "revision": "d2a57ce796d43014475b",
+    "url": "/paper-kit-react-master/static/js/main.d9a488f7.chunk.js"
+  },
+  {
+    "revision": "629521dbbff4f62bbe61",
+    "url": "/paper-kit-react-master/static/js/runtime~main.fbd56c63.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/paper-kit-react-master/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "340ed9601a7508c376c8409fc2948cc5",
+    "url": "/paper-kit-react-master/static/media/background.340ed960.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/paper-kit-react-master/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/paper-kit-react-master/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/paper-kit-react-master/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/paper-kit-react-master/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/paper-kit-react-master/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/paper-kit-react-master/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "1331ba97560cc13103d379a8843ad1dc",
+    "url": "/paper-kit-react-master/static/media/federico-beccari.1331ba97.jpg"
+  },
+  {
+    "revision": "7812184c3710d9623c684956fe048ddd",
+    "url": "/paper-kit-react-master/static/media/fog-low.7812184c.png"
+  },
+  {
+    "revision": "efe053682ec97d330adad93c55dc7bd0",
+    "url": "/paper-kit-react-master/static/media/ilya-yakover.efe05368.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/paper-kit-react-master/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
+    "url": "/paper-kit-react-master/static/media/joshua-stannard.8a26aabc.jpg"
+  },
+  {
+    "revision": "e06d84cb846db2a463ac3d904ac0c362",
+    "url": "/paper-kit-react-master/static/media/kaci-baum-2.e06d84cb.jpg"
+  },
+  {
+    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
+    "url": "/paper-kit-react-master/static/media/landing-page.d65b6316.png"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/paper-kit-react-master/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "6a5ed8e564264d5a571802c9cabfe009",
+    "url": "/paper-kit-react-master/static/media/logo-bitva.6a5ed8e5.svg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/paper-kit-react-master/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/paper-kit-react-master/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/paper-kit-react-master/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/paper-kit-react-master/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "ed8128994a7eed31e8ebeba831ed040c",
+    "url": "/paper-kit-react-master/static/media/profile-page.ed812899.png"
+  },
+  {
+    "revision": "29df8117c07250e9b1ea8e3a67627f67",
+    "url": "/paper-kit-react-master/static/media/soroush-karimi.29df8117.jpg"
+  },
+  {
+    "revision": "fe3a7838dddb1fbf6901af17ca246ea1",
+    "url": "/paper-kit-react-master/static/media/uriel-soberanes.fe3a7838.jpg"
+  }
+]);
