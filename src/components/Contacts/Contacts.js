b5e7b368d796f1af2b0b4d94df672bb884b39187
Anastasia <surikova@dalten.cz>
2020-02-04 20:59:52 +0100

    uprava fotek v sekci fontakt

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Container, Row, Col} from "reactstrap";
+import React, {Component} from 'react'
+import {Container, Row, Col} from "reactstrap"
 
 class Contacts extends Component {
 
@@ -15,31 +15,31 @@ class Contacts extends Component {
                     <div>
                         <Container>
                             <Row>
-                                <Col md="4" sm="6" className="section-centered">
-                                    <Row className="justify-content-md-center"><img className="img-cc img-no-padding img-responsive" alt="Aleš Příhoda"
-                                         src={require("assets/img/contacts/ales.jpg")}/></Row>
-                                    <Row className="justify-content-md-center"><h3>Aleš Příhoda</h3><br></br></Row>
-                                    <Row className="justify-content-md-center">prihoda@engineersprague.cz</Row>
+                                <Col lg="4" className="section-centered">
+                                    <img className="img-cc img-no-padding img-responsive" alt="Aleš Příhoda"
+                                         src={require("assets/img/contacts/ales.jpg")}/>
+                                    <h3>Aleš Příhoda</h3>
+                                    <div>prihoda@engineersprague.cz</div>
                                 </Col>
-                                <Col md="4" sm="6" className="section-centered">
-                                    <Row className="justify-content-md-center"><img className="img-cc img-no-padding img-responsive" alt="Tomáš Hlaváč"
-                                     src={require("assets/img/contacts/tomas.jpg")}/></Row>
-                                    <Row className="justify-content-md-center"><h3>Tomáš Hlaváč</h3></Row>
-                                    <Row className="justify-content-md-center">hlavac@engineersprague.cz</Row>
+                                <Col lg="4" className="section-centered">
+                                    <img className="img-cc img-no-padding img-responsive" alt="Tomáš Hlaváč"
+                                         src={require("assets/img/contacts/tomas.jpg")}/>
+                                    <h3>Tomáš Hlaváč</h3>
+                                    <div>hlavac@engineersprague.cz</div>
                                 </Col>
-                                <Col md="4" sm="6" className="section-centered">
-                                    <Row className="justify-content-md-center"><img className="img-cc img-no-padding img-responsive" alt="Richard Jarošík"
-                                     src={require("assets/img/contacts/richie.jpg")}/></Row>
-                                    <Row className="justify-content-md-center"><h3>Richard Jarošík</h3></Row>
-                                    <Row className="justify-content-md-center">richard.jarosik6520@seznam.cz</Row>
+                                <Col lg="4" className="section-centered">
+                                    <img className="img-cc img-no-padding img-responsive" alt="Richard Jarošík"
+                                         src={require("assets/img/contacts/richie.jpg")}/>
+                                    <h3>Richard Jarošík</h3>
+                                    <div>richard.jarosik6520@seznam.cz</div>
                                 </Col>
                             </Row>
                         </Container>
                     </div>
                 </Container>
             </div>
-        );
+        )
     }
 }
 
-export default Contacts;
+export default Contacts
