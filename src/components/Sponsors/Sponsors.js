73edbee8fae5ce2afe837b4ae3d64b81d765200f
Marek Klement <marek.klement@bcvsolutions.eu>
2020-02-07 14:02:21 +0100

    Added new sponzors

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -31,14 +31,20 @@ class Sponsors extends Component {
                            src={require("assets/img/sponzors/akka.jpg")}/></div>
                       <div class="grid-item"><img className="img-circle img-no-padding img-responsive" alt="backbone"
                            src={require("assets/img/sponzors/ctsport.png")}/></div>
-                      <div class="grid-item"><img className="img-circle img-no-padding img-responsive" alt="AKKA"
-                           src={require("assets/img/sponzors/akka.jpg")}/></div>
+                      <div class="grid-item"><img className="img-circle img-no-padding img-responsive" alt="Enigoo"
+                           src={require("assets/img/sponzors/enigoo.png")}/></div>
                       <div class="grid-item"><img className="img-circle img-no-padding img-responsive" alt="ČZU"
                            src={require("assets/img/sponzors/czu.gif")}/></div>
                       <div class="grid-item-sm"><img className="img-circle img-no-padding img-responsive" alt="VŠE"
                            src={require("assets/img/sponzors/vse.png")}/></div>
                       <div class="grid-item"><img className="img-circle img-no-padding img-responsive" alt="ČVUT"
                            src={require("assets/img/sponzors/cvut.jpeg")}/></div>
+                       <div class="grid-item-li"><img className="img-circle img-no-padding img-responsive" alt="UK"
+                            src={require("assets/img/sponzors/uk-skola.png")}/></div>
+                       <div class="grid-item"><img className="img-circle img-no-padding img-responsive" alt="SU"
+                            src={require("assets/img/sponzors/su.png")}/></div>
+                       <div class="grid-item-li"><img className="img-circle img-no-padding img-responsive" alt="FSP"
+                            src={require("assets/img/sponzors/fsp.png")}/></div>
                     </div>
                 </Container>
             </div>
